e0adebeb6697424c0aa3e6d9b27f72d5d27af78c
HDenizD <denizderezzed@gmail.com>
2024-02-23 11:40:59 +0100

    Add edit mode debounce functionality to ListItem component

--- src/components/todoApp/ListItem.tsx
+++ src/components/todoApp/ListItem.tsx
@@ -7,8 +7,41 @@ export function ListItem({
   item: Todo
   deleteTodo: (id: Todo['id']) => void
 }) {
+  let editModeDebounceCounter = 0
+  let intervalTimer: number
+
+  function toggleEditModeDebouncer() {
+    if (editModeDebounceCounter >= 1) {
+      clearInterval(intervalTimer)
+      return
+    }
+    intervalTimer = setInterval(() => {
+      editModeDebounceCounter++
+      if (editModeDebounceCounter === 1) {
+        console.log('Edit ON!')
+        clearInterval(intervalTimer)
+        return
+      }
+    }, 1000)
+  }
+
+  function abortToggleEditModeDebouncer() {
+    console.log('Edit OFF!')
+    clearInterval(intervalTimer)
+    editModeDebounceCounter = 0
+    return
+  }
+
   return (
-    <li className='flex break-words gap-3 justify-between p-3 outline outline-1 first-of-type:rounded-t last-of-type:rounded-b hover:bg-zinc-800 cursor-pointer items-center'>
+    <li
+      className='flex break-words gap-3 justify-between p-3 outline outline-1 first-of-type:rounded-t last-of-type:rounded-b hover:bg-zinc-800 cursor-pointer items-center'
+      onMouseDown={() => {
+        toggleEditModeDebouncer()
+      }}
+      onMouseUp={() => {
+        abortToggleEditModeDebouncer()
+      }}
+    >
       <span className='break-all select-none'> {item.title}</span>
       <button
         onClick={() => deleteTodo(item.id)}
